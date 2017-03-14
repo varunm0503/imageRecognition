18d3175f7e7982daee96c659c11d4426f572f00f
varunm0503 <varunm0503@gmail.com>
2017-03-14 00:27:42 -0700

    Images converted to vector

---
+++ convert2.py
@@ -0,0 +1,43 @@
+import re
+import numpy
+
+from os import listdir
+from os.path import isfile, join
+
+def convertTestSetToVectors(mypath):
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        vectorlist = []
+        for filename in onlyfiles:
+                imageVector = read_pgm(mypath + '/' + filename)
+                vectorlist.append(imageVector)
+	return vectorlist
+
+
+def read_pgm(filename, byteorder='>'):
+    """Return image data from a raw PGM file as numpy array.
+
+    Format specification: http://netpbm.sourceforge.net/doc/pgm.html
+
+    """
+    with open(filename, 'rb') as f:
+        buffer = f.read()
+    try:
+        header, width, height, maxval = re.search(
+            b"(^P5\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
+    except AttributeError:
+        raise ValueError("Not a raw PGM file: '%s'" % filename)
+    return (numpy.frombuffer(buffer,
+                            dtype='u1' if int(maxval) < 256 else byteorder+'u2',
+                            count=int(width)*int(height),
+                            offset=len(header)
+                            ).reshape((int(height), int(width)))).flatten()
+
+
+if __name__ == "__main__":
+    from matplotlib import pyplot
+    image = read_pgm("foo.pgm", byteorder='<')
+    pyplot.imshow(image, pyplot.cm.gray)
+    pyplot.show()
