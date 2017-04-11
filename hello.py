94c0079e770fe4fe86ed52598ab84c76c3c5a0cd
sid3695 <sid3695@gmail.com>
2017-04-11 17:52:42 +0530

    modified uploading

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import render_template
 from werkzeug.utils import secure_filename
 from flask import Flask, request, redirect, url_for
 
-UPLOAD_FOLDER = './static'
+UPLOAD_FOLDER = './static/'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
@@ -19,11 +19,12 @@ def hello_world():
 def upload_file():
 	if request.method == 'POST':
 		f = request.files['file']
-		f.save(secure_filename(f.filename))
-		imgList = detectFace(secure_filename(f.filename))
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+		imgList = detectFace(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
 		print "hi"
 		print imgList
-		return render_template('afterDetection.html', image1 = secure_filename(f.filename))
+		#return "test"
+		return render_template('afterDetection.html', image1 = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
 		
 if __name__ == '__main__':
 	app.run(debug = True)
