1992f1442c998ca102e6219828d3e5a0df858345
varunm0503 <varunm0503@gmail.com>
2017-04-14 01:26:15 -0700

    Added database for people as in training data. Need to add db to link images in an album

--- hello.py
+++ hello.py
@@ -1,16 +1,29 @@
 import os
+import sqlite3
 from detect import *
 from convert import *
-from flask import render_template
+from flask import render_template, g
 from werkzeug.utils import secure_filename
 from flask import Flask, request, redirect, url_for
 
+DATABASE = '/path/to/database.db'
 UPLOAD_FOLDER = './static/'
 ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+	return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
 
 @app.route('/hello/')
 def hello_world():
@@ -35,7 +48,25 @@ def upload_file():
 			a = train("./resources/our",i)
 			pList.append(a)
 		print pList
-		return render_template('afterDetection.html', image1 = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)), imgList = imageList, pList = pList)
+		con = sqlite3.connect('database.db')
+		con.row_factory = sqlite3.Row
+		cur = con.cursor()
+		print "Opened database successfully"
+		string = "select NAME from user where Id in ("
+		for p in pList:
+			string += str(p)
+			string += ","
+		string = string[:-1]
+		string += ")"
+		print string
+		cur.execute(string)
+		rows = cur.fetchall()
+		print rows
+		myList = []
+		for r in rows:
+			myList.append(r[0])	
+		print myList	
+		return render_template('afterDetection.html', image1 = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)), imgList = imageList, pList = myList)
 		
 if __name__ == '__main__':
 	app.run(debug = True)
